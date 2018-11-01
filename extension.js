a7ecfab95135aa590ef02b275f06120e165bf012
fredricklou523 <fredrick.lou@gmail.com>
2018-10-31 22:22:52 -0700

    Base extension file + activation on right click set up

---
+++ extension.js
@@ -0,0 +1,9 @@
+const vscode = require('vscode');
+
+function activate(context) {
+  console.log('Congratulations, your extension "pomocodev2" is now active!');
+}
+exports.activate = activate;
+
+function deactivate() {}
+exports.deactivate = deactivate;
